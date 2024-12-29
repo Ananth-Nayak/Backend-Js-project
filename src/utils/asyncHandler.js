246dcc0d15485c5e2aa3eb596f6ed1ee659878f7
Ananth Nayak <nananthnayak@gmail.com>
2024-12-29 20:56:52 +0530

    wrapUp fn using promise,api error handling using nodejs Error class

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,7 @@
-// const asyncHandler = () => {}
-// const asyncHandler = (func) => {() => {}}
-// const asyncHandler = (func) => {async () => {}}
-// const asyncHandler = (func) => async () => {}
-
-
-const asyncHandler = ( func ) => async (req,res,next) => {
-  try {
-
-    await func(req,res,next)
-
-  } catch (error){
-    res.status(error.code || 500).json({
-      success: false,
-      message: error,
-    })
+const asyncHandler = (requestHandler) => {
+  (req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next))
+    .catch((err) => next(err))
   }
 }
 
