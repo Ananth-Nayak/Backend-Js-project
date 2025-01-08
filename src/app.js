f8c5871a1c3eb3f64158341cf240d7d6cb13d437
Ananth Nayak <nananthnayak@gmail.com>
2025-01-08 15:25:07 +0530

    fixed issues and added api versioning to user route

--- src/app.js
+++ src/app.js
@@ -19,10 +19,7 @@ import userRouter from './routes/user.routes.js'
 
 
 // routes declaration
-app.use('/users',userRouter);
-// we write like bcz, users can have multiple task(endpoint) like users/login,
-// users/profile or etc.. so we define all the endpoint that related to users in userRouter, and it will be prefix with /users, like /users/register, /users/login,etc..
-// this is to avoid mess in the app.js if we write everything in single place
+app.use('/api/v1/users',userRouter);
 
 
 export { app }
