08525f5f530c1291f02a34da8a865a1af4cf8563
Ananth Nayak <nananthnayak@gmail.com>
2025-01-08 14:40:12 +0530

    defined user router and created a method to handle that userroute

--- src/app.js
+++ src/app.js
@@ -14,4 +14,15 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use('/users',userRouter);
+// we write like bcz, users can have multiple task(endpoint) like users/login,
+// users/profile or etc.. so we define all the endpoint that related to users in userRouter, and it will be prefix with /users, like /users/register, /users/login,etc..
+// this is to avoid mess in the app.js if we write everything in single place
+
+
 export { app }
